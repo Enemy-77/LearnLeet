112e3c97799caf0fef964866690d4cb7725e9d46
giantdig <1914118343@qq.com>
2019-12-30 23:32:38 +0800

    feature : symmetric

--- leet_code/SameTree.h
+++ leet_code/SameTree.h
@@ -6,7 +6,6 @@
 
 
 struct TreeNode {
-	TreeNode() = default;
      int val;
      TreeNode *left;
      TreeNode *right;
@@ -24,4 +23,19 @@ bool isSameTree(TreeNode* p, TreeNode* q)
 		return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
 	}
 	return false;
+}
+
+bool isMirror(TreeNode* tree1, TreeNode* tree2)
+{
+	if (tree1 == nullptr && tree2 == nullptr)
+		return true;
+	if (tree1 == nullptr || tree2 == nullptr)
+		return false;
+	return tree1->val == tree2->val && isMirror(tree1->left, tree2->right) && isMirror(tree1->right, tree2->left);
+
+}
+
+bool isSymmetric(TreeNode* root)
+{
+	return isMirror(root, root);
 }
